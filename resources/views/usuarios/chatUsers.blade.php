8d88308ecfd9ab78eaaf62d2cef2e39821dd952b
Gasper07 <gasper0706@gmail.com>
2017-04-17 18:14:57 -0500

    fefe

--- resources/views/usuarios/chatUsers.blade.php
+++ resources/views/usuarios/chatUsers.blade.php
@@ -106,9 +106,9 @@
               <p class="gasper"> {{ $banderaOnline = 0}}</p>
               @foreach($AllOnlineUser as $onlineUsers)
                 <p class="gasper"> {{ $banderaOnline = $banderaOnline+1 }}</p>
-                <div class="captionCircleUser captionDenoews">
-                  <a href="#!" class="userLive" data-idonline='{{ $onlineUsers['id_user'] }}'>
-                    <div class="label dataPrubeIm vloqImageUser dataProfileAllUsersOnline" style="background-image: url('http://app-7983e06f-f506-428d-aef9-aea82667c6d7.cleverapps.io/public/assets/profiles/{{ $onlineUsers['foto'] }}')">
+                <div class="captionCircleUser captionDenoews AlluserReegitradosPorBloque">
+                  <a href="#!" class="userLive" data-idonline="{{ $onlineUsers['id_user'] }}" data-iduserchat="{{ $onlineUsers['id_user'] }}"> 
+                    <div class="label dataPrubeIm vloqImageUser dataProfileAllUsersOnline styRos6" style="background-image: url('http://app-7983e06f-f506-428d-aef9-aea82667c6d7.cleverapps.io/public/assets/profiles/{{ $onlineUsers['foto'] }}');"> 
                     </div>
                   </a>
                 </div>
@@ -126,10 +126,10 @@
               @foreach($AllOnlineUser as $onlineUsers)
                 <p class="gasper"> {{ $banderaOnline = $banderaOnline+1 }}</p>                  
                 @if($banderaOnline >= 4 )
-                  <div class="captionCircleUser captionDenoews">
-                    <a href="#!" class="userLive" data-idonline='{{ $onlineUsers['id_user'] }}'>
-                      <div class="label dataPrubeIm vloqImageUser dataProfileAllUsersOnline" style="background-image: url('http://app-7983e06f-f506-428d-aef9-aea82667c6d7.cleverapps.io/public/assets/profiles/{{ $onlineUsers['foto'] }}')">
-                      </div>            
+                  <div class="captionCircleUser captionDenoews AlluserReegitradosPorBloque">
+                    <a href="#!" class="userLive" data-idonline="{{ $onlineUsers['id_user'] }}" data-iduserchat="{{ $onlineUsers['id_user'] }}"> 
+                      <div class="label dataPrubeIm vloqImageUser dataProfileAllUsersOnline styRos6" style="background-image: url('http://app-7983e06f-f506-428d-aef9-aea82667c6d7.cleverapps.io/public/assets/profiles/{{ $onlineUsers['foto'] }}');"> 
+                      </div>
                     </a>
                   </div>
                 @endif

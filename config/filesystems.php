0f902020ef10101e6c86d16f36f777f486c06b99
Gasper07 <gasper0706@gmail.com>
2017-04-18 18:10:24 -0500

    de

--- config/filesystems.php
+++ config/filesystems.php
@@ -63,7 +63,7 @@ return [
             'driver' => 's3',
             'key' => 'TLCQ8T7MCYDE2CC7OMGY',
             'secret' => 'eKBAGapaeNvyEwsrGG-B3NGYp2oeWWX1_7CxSg==',
-            'region' => 'us-east-1',
+            'region' => 'eu-west-1',
             'bucket' => 'cloudfront.amazonaws.com',
         ],
 

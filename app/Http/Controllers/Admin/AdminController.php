fbd7da5ab4cf201b83afd26281cc36351019b4fa
toretto07 <edgardo_fb22@hotmail.com>
2017-04-19 02:40:13 -0500

    fre

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2967,11 +2967,10 @@ class AdminController extends Controller
 
       #Si la descarga proviene del primer folder que seleccione
       if($fileUrl == '' && $fileUrl2 == ''&& $fileUrl3 == '' && $fileUrl4 == '' && $fileUrl5 == ''){
-        $move = \Storage::disk('ubUploadsChange')->put($nombreDocumento,  \File::get($fileDocumento));
-        $route = \Storage::disk('ubUploadsChange');
-        print_r($route);
-
-        \Storage::move('/'.$nombreDocumento.'', 'uploadss/'.$nombreDocumento.'');
+        $move = \Storage::disk('ubUploadsChange')->put('uploadss/'.$nombreDocumento,  \File::get($fileDocumento));
+        // $route = base_path();
+        // print_r($route);
+        // \Storage::move('/'.$nombreDocumento.'', 'uploadss/'.$nombreDocumento.'');
         Session::flash('Upload_document', "El Archivo ha sido subido con exito");
         return back()->withInput();
       }

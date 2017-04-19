32092db1334178235e230e7e6c8373ab3dac3369
toretto07 <edgardo_fb22@hotmail.com>
2017-04-19 03:35:32 -0500

    gtr

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -1045,9 +1045,11 @@ class AdminController extends Controller
       $idurl4 = '';
       $idurl5 = '';
       #directorios de archivos
-      $ArchivosCarpetas = base_path().'/public/assets/images/documents-admin';      
+      // $ArchivosCarpetas = base_path().'/public/assets/images/documents-admin';      
+      $ArchivosCarpetas = \Storage::disk('ubUploadsChange')->get();      
       #get archivos que contiene la carpeta
       $getDirectoryArchivos = \File::files($ArchivosCarpetas);
+      dd($getDirectoryArchivos);
 
       #get directorios carpetas
       $getDirectoryCarpetas = \File::directories($ArchivosCarpetas);
@@ -2968,9 +2970,6 @@ class AdminController extends Controller
       #Si la descarga proviene del primer folder que seleccione
       if($fileUrl == '' && $fileUrl2 == ''&& $fileUrl3 == '' && $fileUrl4 == '' && $fileUrl5 == ''){
         $move = \Storage::disk('ubUploadsChange')->put('uploadss/'.$nombreDocumento,  \File::get($fileDocumento));
-        $route = base_path();
-        // print_r($route);
-        \Storage::move('bucket-1237ad59-1025-48ba-90c5-d567e8ecd3dc-fsbucket.services.clever-cloud.com/'.$nombreDocumento.'', 'uploadss/'.$nombreDocumento.'');
         Session::flash('Upload_document', "El Archivo ha sido subido con exito");
         return back()->withInput();
       }

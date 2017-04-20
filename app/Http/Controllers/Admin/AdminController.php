4055935739dcd2af91b69f589e3d28790ced571c
Gasper07 <gasper0706@gmail.com>
2017-04-20 10:26:49 -0500

    ref

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2912,7 +2912,8 @@ class AdminController extends Controller
         #data
         // return (new \Response($storagePath, 200))
         //               ->header('Content-Type', $entry->mime);
-        return response($storagePath, 200, ['Content-Type' => $entry->mime]);
+        return response()->download($storagePath);
+        // return response($storagePath, 200, ['Content-Type' => $entry->mime]);
         // $fileData = \Storage::disk('ubUploadsChange')->get($entry->nombre_archivo);
         // dd($storagePath);
 

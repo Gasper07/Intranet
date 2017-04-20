356c6754cf150f03a3cd26703b3b5c85c2373de3
Gasper07 <gasper0706@gmail.com>
2017-04-20 14:40:01 -0500

    fre

--- resources/views/usuarios/partials/posts/fields-tipo-cumple-impar.blade.php
+++ resources/views/usuarios/partials/posts/fields-tipo-cumple-impar.blade.php
@@ -5,7 +5,8 @@
         <i class="fa fa-times" aria-hidden="true"></i>
       </a>  --}}
       <div class="bgHappy">
-        <img class="img-responsive PeopleHappy" src="http://app-7983e06f-f506-428d-aef9-aea82667c6d7.cleverapps.io/public/assets/profiles/{{ $dataUSe->foto }}">
+        <p class="gasper">{{ $GetImage  = \Storage::disk('ubUploadsChange')->get('/profiles/'.$dataUSe->foto.'') }}</p>
+        <img class="img-responsive PeopleHappy" src="background-image: url('data:{{ $PostImpar['mime'] }};base64,{{ base64_encode($GetImage) }}')">
       </div>
       <h4 class="fontCovered">¡Feliz cumpleaños {{ $dataUSe->name }}!</h4>
       <h5 class="fontMiriamProRegular">{{ $PostImpar['descripcion'] }}</h5>
@@ -51,7 +52,8 @@
           @if($PostImpar['id'] == $datComents->id_publicacion)
             <div class="ui feed uifeedComnetUser">
               <div class="event">
-                <div class="label dataPrubeIm" style="background-image: url('http://app-7983e06f-f506-428d-aef9-aea82667c6d7.cleverapps.io/public/assets/profiles/{{ $datComents->foto }}')">
+                <p class="gasper">{{ $GetImage  = \Storage::disk('ubUploadsChange')->get('/profiles/'.$datComents->foto.'') }}</p>
+                <div class="label dataPrubeIm" style="background-image: url('data:{{ $PostImpar['mime'] }};base64,{{ base64_encode($GetImage) }}')">
                 </div>
                 <div class="content">
                   <div class="summary">

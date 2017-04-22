6d19a61850840ce8dc71e633c735ce00c28692b0
toretto07 <edgardo_fb22@hotmail.com>
2017-04-22 00:03:53 -0500

    dew

--- resources/views/usuarios/partials/fields-galeria-fotos-user.blade.php
+++ resources/views/usuarios/partials/fields-galeria-fotos-user.blade.php
@@ -2,23 +2,21 @@
   <h3>Galería de fotos</h3>
   @foreach($arrayOfImages as $DataImages => $valuDateImages)
     <p class="gasper">{{ $canditdadImagesForArray = count($DataImages) }}</p>
-    @for($ga=0; $ga < $canditdadImagesForArray; $ga++ )
-      <p class="gasper">{{ $GetImage  = \Storage::disk('ubUploadsChange')->get('/documents/'.$DataImages[$ga]['imagesGalery'].'') }}</p>
-      <img class="img-responsive clImgView" src="data:{{ $DataImages[$ga]['mime'] }};base64,{{ base64_encode($GetImage) }}" alt=""  data-toggle="modal" data-target="#myModalswPost">
+      <p class="gasper">{{ $GetImage  = \Storage::disk('ubUploadsChange')->get('/documents/'.$DataImages['imagesGalery'].'') }}</p>
+      <img class="img-responsive clImgView" src="data:{{ $DataImages['mime'] }};base64,{{ base64_encode($GetImage) }}" alt=""  data-toggle="modal" data-target="#myModalswPost">
         
       <!-- Modal -->
       <div class="modal fade" id="myModalswPost" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
         <div class="modal-dialog dialoDataImgen" role="document">
           <div class="modal-content">
             <div class="modal-body">
-              <img class="img-responsive" src="data:{{ $DataImages[$ga]['mime'] }};base64,{{ base64_encode($GetImage) }}" alt="">
+              <img class="img-responsive" src="data:{{ $DataImages['mime'] }};base64,{{ base64_encode($GetImage) }}" alt="">
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
             </div>
           </div>
         </div>
-      </div>
-    @endfor              
+      </div>           
   @endforeach
 </div>

1b4b91eaccbd5f3b07c106ec84a00df1a96df266
toretto07 <edgardo_fb22@hotmail.com>
2017-04-21 20:17:27 -0500

    fre

--- resources/views/usuarios/partials/fields-galeria-fotos-user.blade.php
+++ resources/views/usuarios/partials/fields-galeria-fotos-user.blade.php
@@ -3,14 +3,15 @@
   @foreach($arrayOfImages as $DataImages)
     <p class="gasper">{{ $canditdadImagesForArray = count($DataImages) }}</p>
     @for($ga=0; $ga < $canditdadImagesForArray; $ga++ )
-      <img class="img-responsive clImgView" src="http://app-7983e06f-f506-428d-aef9-aea82667c6d7.cleverapps.io/public/assets/images/documents/{{ $DataImages[$ga] }}" alt="{{ $DataImages[$ga] }}"  data-toggle="modal" data-target="#myModalswPost">
+      <p class="gasper">{{ $GetImage  = \Storage::disk('ubUploadsChange')->get('/profiles/'.$DataImages[$ga].'') }}</p>
+      <img class="img-responsive clImgView" src="data:{{ $PostImpar['mime'] }};base64,{{ base64_encode($GetImage) }}" alt=""  data-toggle="modal" data-target="#myModalswPost">
         
       <!-- Modal -->
       <div class="modal fade" id="myModalswPost" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
         <div class="modal-dialog dialoDataImgen" role="document">
           <div class="modal-content">
             <div class="modal-body">
-              <img class="img-responsive" src="http://app-7983e06f-f506-428d-aef9-aea82667c6d7.cleverapps.io/public/assets/images/documents/{{ $DataImages[$ga] }}" alt="{{ $DataImages[$ga] }}">
+              <img class="img-responsive" src="data:{{ $PostImpar['mime'] }};base64,{{ base64_encode($GetImage) }}" alt="">
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
